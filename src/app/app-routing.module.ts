6c6db66bc93f009bf24e06205f05308106aba041
orianeLalo <95689944+orianeLalo@users.noreply.github.com>
2022-05-19 16:15:45 +0200

    demandes de trajets passager visibles + possibilité de les annuler

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AddRideComponent } from './add-ride/add-ride.component';
 import { DemandsReceivedComponent } from './demands-received/demands-received.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { PassengerDemandsComponent } from './passenger-demands/passenger-demands.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ResultsComponent } from './results/results.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path:'signup', component: SignUpComponent},
   { path:'profile', component: ProfileComponent },
   { path:'addride', component: AddRideComponent },
-  { path:'demands-received', component: DemandsReceivedComponent }
+  { path:'demands-received', component: DemandsReceivedComponent },
+  { path:'passenger-demands', component: PassengerDemandsComponent}
 ];
 
 @NgModule({
